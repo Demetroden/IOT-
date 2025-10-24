c8a07d654af3601bd3793432faf6ea3122f46fa2
Demetroden <xemetroden@gmail.com>
2025-10-24 21:59:03 +0400

    first commit

---
+++ HW1/task_2.c
@@ -0,0 +1,13 @@
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("Let's\n");
+	printf("\t go\n");
+	printf("\t\t to walk");
+
+	return 0;
+}
+
---
+++ HW1/task_3.c
@@ -0,0 +1,16 @@
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("\t   *\n");
+	printf("\t  ***\n");
+	printf("\t *****\n");
+	printf("\t*******\n");
+	printf("\t HHOHH\n");
+	printf("\t ZZZZZ\n");
+
+	return 0;
+}
+
