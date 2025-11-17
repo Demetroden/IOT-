599e7fb4fac8cba4c063ab1ebd8cf3c44e849ff0
Demetroden <xemetroden@gmail.com>
2025-11-17 22:56:16 +0400

    B20

--- Base C/C20.c
+++ Base C/C20.c
@@ -1,30 +1,22 @@
-
 #include <stdio.h>
-
-
-
 void skobka()
 {
 	char input;
-	int i,j = 0;
+	int i=0,j = 0;
 	while((input = getchar()) != '.')
 	{
 		
 		if(input == '(' || input == ')') 
 		{
-			
+			if(i==0 && j > 0 )
+				break;
 			input == '(' ? i++ : j++;
 			
 		}
 		
 	}
 	i==j ? printf("YES"):printf("NO");
-		
-	
 }
-
-
-
 int main(int argc, char **argv)
 {
 
